203b97b4edc5095d0435e03a109121f0863a52f0
PHAM_VAN_NHAT <nhaterik@gmail.com>
2024-05-18 16:51:32 +0700

    Tìm số lớn nhất trong xâu

---
+++ 41.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Sstring
+{
+private:
+    string s1;
+
+public:
+    void input()
+    {
+        cin >> s1;
+    }
+    void solve()
+    {
+        string n = "";
+        int res = 0;
+        for (int i = 0; i < s1.size(); i++)
+        {
+            if (isdigit(s1[i]))
+                n += s1[i];
+            else if (n != "")
+            {
+                res = max(res, stoi(n));
+                n = "";
+            }
+        }
+        if (n != "")
+            res = max(res, stoi(n));
+
+        cout << res << endl;
+    }
+};
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        Sstring x;
+        x.input();
+        x.solve();
+    }
+
+    return 0;
+}
