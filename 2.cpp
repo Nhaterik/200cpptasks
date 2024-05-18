f371296e598fcb621634abde9faacf2fe9aae3b4
PHAM_VAN_NHAT <nhaterik@gmail.com>
2024-05-18 09:42:12 +0700

    CPP0738 - CỘNG 1 VÀ NHÂN ĐÔI

---
+++ 2.cpp
@@ -0,0 +1,69 @@
+1 // Input:
+
+// 2
+// 3
+// 16 16 16
+// 2
+// 2 3
+
+// Output:
+
+// 7
+// 4
+#include <bits/stdc++.h>
+    using namespace std;
+class SumandDouhble
+{
+private:
+    int n, cn = 0;
+    vector<int> v;
+
+public:
+    void solve()
+    {
+        cin >> n;
+        v = vector<int>(n);
+        for (int i = 0; i < n; i++)
+            cin >> v[i];
+        while (true)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                if (v[i] % 2)
+                {
+                    cn++;
+                    v[i]--;
+                }
+            }
+            cn++;
+            int ok = 0;
+            for (int i = 0; i < n; i++)
+            {
+                v[i] /= 2;
+            }
+            for (int i = 0; i < n; i++)
+            {
+                if (v[i])
+                {
+                    ok = 1;
+                    break;
+                }
+            }
+            if (!ok)
+                break;
+        }
+        cout << cn - 1 << endl;
+    }
+};
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        SumandDouhble x;
+        x.solve();
+    }
+
+    return 0;
+}
