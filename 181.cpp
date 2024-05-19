1e3c07cfb8fee4d6818eb0b1f6013479638dfe7f
PHAM_VAN_NHAT <nhaterik@gmail.com>
2024-05-19 09:52:40 +0700

    tìm kiếm trong dãy sắp xếp vòng

---
+++ 181.cpp
@@ -0,0 +1,26 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n, x;
+        cin >> n >> x;
+        vector<int> v;
+        for (int i = 0; i < n; i++)
+        {
+            int x;
+            cin >> x;
+            v.push_back(x);
+        }
+        auto it = find(v.begin(), v.end(), x) - v.begin();
+        cout << it + 1 << endl;
+    }
+
+    return 0;
+}
